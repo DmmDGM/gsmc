60cd83d96b6504cdb8c316f5bd7b4c038e4b15ff
DmmDGM <dm12332131mdgaming@gmail.com>
2025-08-20 20:22:47 -0400

    populated s4 and s5

--- client/src/pages/seasons/s4.tsx
+++ client/src/pages/seasons/s4.tsx
@@ -1,3 +1,61 @@
+// Imports css
+import "./s4.css";
+
+// Defines content
 export function S4() {
-    return <div>s4</div>;
+    // Creates content
+    return <div id="s4">
+        <section>
+            <h3>It's Mods Time!</h3>
+            <p>
+                Season 4 is the first modded season in Geesecraft history with the introduction of Tech Reborn, Applied Energistics 2, and Ad Astra.
+            </p>
+            <p>
+                It's a fresh experience.
+                After all, for the past 3 seasons, we have only been playing vanilla Minecraft with only minimal quality-of-life plugins at most.
+                This time, we got internet with DmmD's ME Network, power with iiPython's generator in the basement, space colonization with rocket science, and McGeese - powered by corn.
+            </p>
+        </section>
+        <section>
+            <h3>∞ Infinite UU Matter ∞</h3>
+            <p>
+                Tech Reborn has this "UU Matter" that you can use to craft literally most everything in Minecraft.
+                Diamonds? Netherite? Your neighbor's dog? The Backrooms? UU Matter can make it all.
+                Of course, being this overpowered, it is also expensive as hell.
+            </p>
+            <p>
+                Fortunately, we have "technology"!
+                We just converted cobblestone from a cobblestone generator into infinite scraps, and with those scraps and a f- ton of power, we finally had the ability to produce infinite UU Matter!
+                Finally, capitalism at work!
+            </p>
+        </section>
+        <section>
+            <h3>Cool Videos that DmmD Made?</h3>
+            <p>
+                Here are some cool videos that DmmD recorded in this season.
+            </p>
+            <div>
+                <iframe width="540" height="304" src="https://www.youtube.com/embed/iSgJp98DL60" title="Geesecraft Season 4 - End Fight" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> 
+            </div>
+            <div>
+                <iframe width="540" height="304" src="https://www.youtube.com/embed/pS_-XAJgWIE" title="Cat Vibe" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+            <p>
+                The cat vibe is pretty nice. .w.
+            </p>
+        </section>
+        <section>
+            <h3>End of Season 4.</h3>
+            <p>
+                I mean, we didn't lie when we said Geesecraft was supposed to be an annual event.
+                It ran while we were on summer break, and now that school was about to start, the season's gotta end.
+            </p>
+            <p>
+                Feel free to check out our archive or explore our gallery below.
+            </p>
+            <p>
+                #Geesecraft!
+            </p>
+        </section>
+    </div>;
 }
--- client/src/pages/seasons/s5.tsx
+++ client/src/pages/seasons/s5.tsx
@@ -1,3 +1,57 @@
+// Imports css
+import "./s5.css";
+
+// Defines content
 export function S5() {
-    return <div>s5</div>;
+    // Creates content
+    return <div id="s5">
+        <section>
+            <h3>More Mods! More Features! More Chaos!</h3>
+            <p>
+                New year, new Geesecraft, new mods!
+                This time, we added a whole new array of mods, like Cobblemon, Hephaestus, Traveler's Backpack, Mine Cells, and Sculk Depths, just to name a few.
+            </p>
+        </section>
+        <section>
+            <h3>The Basement Tech. Inc.</h3>
+            <p>
+                What's a Geesecraft without over-engineered industralization?
+                That's why we didn't bother to build a house.
+                Our basement IS our house!
+            </p>
+            <p>
+                We got a huge chain of industral machines running deep down under our base manufacturing weird chemicals and chips to power our extremely hungry internet.
+                Everywhere you look, you can see strange multi-block builds, high-voltage wires, and bronze water pipes running all across the room.
+            </p>
+            <p>
+                Welp, that's the Geesecraft mindset for you.
+                Need anything? Just build an entire automatic supply chain to make the automatic harvesting system to construct the automatic crafting system to automatically supply the item you requested on our internet.
+            </p>
+        </section>
+        <section>
+            <h3>Pokemons Everywhere!</h3>
+            <p>
+                Since we added Cobblemon into our server, there were constantly creatures roaming around in our base, distracting us from work or blocking our paths.
+            </p>
+            <p>
+                It's supposed to be a fun feature, but after the initial hype, no one seemed to be interested in catching and leveling up their pokemons.
+                In the end, we started using those random pokemons as training dummies to test the stats of our weapons and armors.
+            </p>
+        </section>
+        <section>
+            <h3>Farewell, Season 5!</h3>
+            <p>
+                Season 5 didn't have a dramatic ending like all previous other seasons.
+                Every member on the server gradually started becoming too busy to log onto Geesecraft.
+                It's unfortunate, but life's just that ruthless.
+                With college beginning for iiPython and DmmD, this season marks the end of happy childhood and the start of real life struggles.
+            </p>
+            <p>
+                #Geesecraft
+            </p>
+            <p>
+                Feel free to check out our archive or explore our gallery below.
+            </p>
+        </section>
+    </div>;
 }
